a76da9bcc711fd1f9cb91187cdbb0af8a872f19b
Akshay Wani <116380320+akshaywani1382004@users.noreply.github.com>
2025-04-26 03:11:09 +0530

    Update graph.cpp

--- graph.cpp
+++ graph.cpp
@@ -2,44 +2,66 @@
 #include <vector>
 using namespace std;
 
-class Graph {
-    int V;
-    vector<vector<int>> adj;
-
-public:
-    Graph(int vertices) {
-        V = vertices;
-        adj.resize(V);
+class graph{
+    vector<vector<int>> adjMatrix;
+    vector<vector<int>> adjList;
+    int edges, vertex;
+    
+    public:
+    graph(int edge, int vertex){
+        edges = edge;
+        this -> vertex = vertex;
+        adjList.resize(vertex);
+        adjMatrix.resize(vertex, vector<int>(vertex,0));
     }
 
-    void addEdge(int u, int v) {
-        adj[u].push_back(v);
-        adj[v].push_back(u);
-    }
+    void input(){
+        int a,b;
+        for(int i = 0; i<edges; i++){
+            cout<<"Enter the Edge Points : ";
+            cin>>a>>b;
+            cout<<endl;
 
-    void display() {
-        for (int i = 0; i < V; i++) {
-            cout << "Node " << i << " -> ";
-            for (int neighbor : adj[i]) {
-                cout << neighbor << " ";
-            }
-            cout << endl;
+            adjMatrix[a][b] = 1;
+            adjMatrix[b][a] = 1;
+
+            adjList[a].push_back(b);
+            adjList[b].push_back(a);
         }
+    }
+
+    void display_adjList(){
+        cout<<"Adjacent List : "<<endl;
+        for(int i = 0; i<vertex ; i++){
+            cout<<i<<" >> ";
+            int j = 0;
+            for(int x : adjList[i]){
+                cout << x << " ";
+            }
+            cout<<endl;
+        }
+        cout<<endl;
+    }
+
+    void display_adjMatrix(){
+        cout<<"Adjacent Matrix : "<<endl;
+        for(int i = 0; i<vertex ; i++){
+            for(int j = 0; j<vertex ; j++){
+                cout<<adjMatrix[i][j] << " ";
+            }
+            cout<<endl;
+        }
+        cout<<endl;
     }
 };
 
-int main() {
-    Graph g(5);
+int main(){
 
-    g.addEdge(0, 1);
-    g.addEdge(0, 4);
-    g.addEdge(1, 2);
-    g.addEdge(1, 3);
-    g.addEdge(1, 4);
-    g.addEdge(2, 3);
-    g.addEdge(3, 4);
+    graph g(3,3);
 
-    g.display();
+    g.input();
+    g.display_adjList();
+    g.display_adjMatrix();
 
     return 0;
 }
